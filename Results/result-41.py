be00ea89a881c4bf659f1f7a3174dcd2311e4fbd
l-r-w-250111 <194588666+l-r-w-250111@users.noreply.github.com>
2026-01-14 22:57:49 +0900

    Add result-41.py with health data analysis

---
+++ Results/result-41.py
@@ -0,0 +1,16 @@
+============================================================================================================================================
+Path Candidate                 | Sync     | L  | Potent   | Raw   | Interlock
+--------------------------------------------------------------------------------------------------------------------------------------------
+Tuberculosis -> Visit to Asia  |    0.154 | 1  |    0.754 | No    | Yes
+Lung Cancer -> Smoking         |   -0.317 | 1  |    0.283 | No    | No
+Lung Cancer -> Bronchitis      |   -0.031 | 1  |    0.569 | Yes   | Yes
+Visit to Asia -> Either        |   -0.195 | 1  |    0.405 | Yes   | Yes
+Smoking -> Either              |    0.103 | 1  |    0.703 | Yes   | Yes
+Either -> X-ray                |    0.118 | 1  |    0.718 | Yes   | Yes
+Either -> Dyspnea              |   -0.020 | 1  |    0.580 | Yes   | Yes
+Bronchitis -> Dyspnea          |    0.068 | 1  |    0.668 | Yes   | Yes
+Lung Cancer -> X-ray           |    0.033 | 3  |    0.420 | No    | Yes
+Smoking -> X-ray               |   -0.081 | 2  |    0.375 | No    | Yes
+Visit to Asia -> X-ray         |   -0.021 | 2  |    0.436 | To    | Yes
+Smoking -> Dyspnea             |   -0.016 | 2  |    0.441 | No    | Yes
+Dyspnea -> Bronchitis          |    0.068 | ∞  |   -0.532 | No    | No
